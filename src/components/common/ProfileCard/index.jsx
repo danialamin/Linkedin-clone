f610a29e5458d8464b5ba12d88bd48c28852b5ea
Danial Amin <danialamin489@gmail.com>
2024-01-24 02:36:59 +0500

    creating profile page

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -1,56 +1,153 @@
 /* eslint-disable react/prop-types */
-import React, {useState} from "react";
-import "./index.scss"
-import ProfileEdit from "../ProfileEdit/index"
-import { getStatus } from "../../../api/FirestoreAPI"
-import { getSingleStatus, getSingleUser } from "../../../api/FirestoreAPI"
-import { useMemo } from "react";
+import React, { useState, useMemo } from "react";
+import { getSingleStatus, getSingleUser } from "../../../api/FirestoreAPI";
 import PostsCard from "../PostsCard";
+import { HiOutlinePencil } from "react-icons/hi";
+import { useLocation } from "react-router-dom";
+import FileUploadModal from "../FileUploadModal";
+import { uploadImage as uploadImageAPI } from "../../../api/ImageUpload";
+import "./index.scss";
 
-export default function ProfileCard({currentUser, onEdit}) {
-  const [allStatuses, setAllStatus] = useState([])
-  const [currentProfile, setCurrentProfile] = useState({})
+export default function ProfileCard({ onEdit, currentUser }) {
+  let location = useLocation();
+  const [allStatuses, setAllStatus] = useState([]);
+  const [currentProfile, setCurrentProfile] = useState({});
+  const [currentImage, setCurrentImage] = useState({});
+  const [progress, setProgress] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
+  const getImage = (event) => {
+    setCurrentImage(event.target.files[0]);
+  };
+  console.log(currentProfile);
+  const uploadImage = () => {
+    uploadImageAPI(
+      currentImage,
+      currentUser.id,
+      setModalOpen,
+      setProgress,
+      setCurrentImage
+    );
+  };
 
   useMemo(() => {
     if (location?.state?.id) {
-      getSingleStatus(setAllStatus, location?.state?.id)
+      getSingleStatus(setAllStatus, location?.state?.id);
     }
+
     if (location?.state?.email) {
-      getSingleUser(setCurrentProfile, location?.state?.email)
+      getSingleUser(setCurrentProfile, location?.state?.email);
     }
-  }, [])
+  }, []);
+
   return (
-  <>
-    <div className="profile-card">
-      <div className="edit-btn">
-        <button onClick={onEdit}>Edit</button>
-      </div>
-      <div className="profile-info">
-        <div>
-          <h3 className="userName">{currentUser.name}</h3>
-          <p className="userEmail">{currentUser.email}</p>
-          <p className="heading">{currentUser.headline}</p>
-          <p>{currentUser.location}</p>
-        </div>
+    <>
+      <FileUploadModal
+        getImage={getImage}
+        uploadImage={uploadImage}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        currentImage={currentImage}
+        progress={progress}
+      />
+      <div className="profile-card">
+        {currentUser.id === location?.state?.id ? (
+          <div className="edit-btn">
+            <HiOutlinePencil className="edit-icon" onClick={onEdit} />
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="profile-info">
+          <div>
+            <img
+              className="profile-image"
+              onClick={() => setModalOpen(true)}
+              src={
+                Object.values(currentProfile).length === 0
+                  ? currentUser.imageLink
+                  : currentProfile?.imageLink
+              }
+              alt="profile-image"
+            />
+            <h3 className="userName">
+              {Object.values(currentProfile).length === 0
+                ? currentUser.name
+                : currentProfile?.name}
+            </h3>
+            <p className="heading">
+              {Object.values(currentProfile).length === 0
+                ? currentUser.headline
+                : currentProfile?.headline}
+            </p>
+            {(currentUser.city || currentUser.country) &&
+            (currentProfile?.city || currentProfile?.country) ? (
+              <p className="location">
+                {Object.values(currentProfile).length === 0
+                  ? `${currentUser.city}, ${currentUser.country} `
+                  : `${currentProfile?.city}, ${currentUser.country}`}
+              </p>
+            ) : (
+              <></>
+            )}
+            {currentUser.website || currentProfile?.website ? (
+              <a
+                className="website"
+                target="_blank"
+                href={
+                  Object.values(currentProfile).length === 0
+                    ? `${currentUser.website}`
+                    : currentProfile?.website
+                } rel="noreferrer"
+              >
+                {Object.values(currentProfile).length === 0
+                  ? `${currentUser.website}`
+                  : currentProfile?.website}
+              </a>
+            ) : (
+              <></>
+            )}
+          </div>
 
-        <div className="right-info">
-          <p className="college">{currentUser.college}</p>
-          <p className="company">{currentUser.company}</p>
+          <div className="right-info">
+            <p className="college">
+              {Object.values(currentProfile).length === 0
+                ? currentUser.college
+                : currentProfile?.college}
+            </p>
+            <p className="company">
+              {Object.values(currentProfile).length === 0
+                ? currentUser.company
+                : currentProfile?.company}
+            </p>
+          </div>
         </div>
+        <p className="about-me">
+          {Object.values(currentProfile).length === 0
+            ? currentUser.aboutMe
+            : currentProfile?.aboutMe}
+        </p>
+
+        {currentUser.skills || currentProfile?.skills ? (
+          <p className="skills">
+            <span className="skill-label">Skills</span>:&nbsp;
+            {Object.values(currentProfile).length === 0
+              ? currentUser.skills
+              : currentProfile?.skills}
+          </p>
+        ) : (
+          <></>
+        )}
       </div>
 
-      <div className="profile-status-main">
-        {allStatuses.filter((item) => {
-          return item.userEmail === localStorage.getItem("userEmail")
-        }).map((posts) => {
+      <div className="post-status-main">
+        {allStatuses?.map((posts) => {
           return (
             <div key={posts.id}>
-              <PostsCard posts={posts}/>
+              <PostsCard posts={posts} />
             </div>
-          )
+          );
         })}
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
--- src/components/common/ProfileEdit/index.jsx
+++ src/components/common/ProfileEdit/index.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import "./index.scss"
+import { AiOutlineClose } from "react-icons/ai"
 import { editProfile } from '../../../api/FirestoreAPI'
 import { useState } from 'react'
 
 export default function ProfileEdit({onEdit, currentUser}) {
-  const [editInputs, setEditInputs] = useState({})
+  const [editInputs, setEditInputs] = useState({currentUser})
   const getInput = (event) => {
     setEditInputs((prev) => { return({...prev, [event.target.name]: event.target.value})})
   }
@@ -16,14 +17,82 @@ export default function ProfileEdit({onEdit, currentUser}) {
   return (
     <div className='profile-card'>
       <div className="edit-btn">
-        <button onClick={onEdit}>Edit</button>
+        <AiOutlineClose className="close-icon" onClick={onEdit} size={25} />
       </div>
       <div className='profile-edit-inputs'>
-        <input onChange={getInput} className="common-input" name="name" placeholder='Name' />
-        <input onChange={getInput} className="common-input" name="location" placeholder='Location' />
-        <input onChange={getInput} className="common-input" name="headline" placeholder='Bio' />
-        <input onChange={getInput} className="common-input" name="company" placeholder='Company' />
-        <input onChange={getInput} className="common-input" name="college" placeholder='College' />
+        
+        <label htmlFor="">Name</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="name" 
+        placeholder='Name'
+        value={editInputs.name} />
+
+        <label htmlFor="">Bio</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="headline" 
+        placeholder='Bio' 
+        value={editInputs.headline}/>
+
+        <label htmlFor="">Country</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="country" 
+        placeholder='Country' 
+        value={editInputs.country} />
+
+        <label htmlFor="">City</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="city" 
+        placeholder='City' 
+        value={editInputs.city} />
+
+        <label htmlFor="">Company</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="industry" 
+        placeholder='Industry' 
+        value={editInputs.industry}/>
+
+        <label htmlFor="">College</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="college" 
+        placeholder='College' 
+        value={editInputs.college}/>
+
+        <label htmlFor="">Website</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="website" 
+        placeholder='Website' 
+        value={editInputs.website} />
+
+        <label htmlFor="">About</label>
+        <textarea 
+        name="aboutMe" 
+        className='common-textArea'
+        onChange={getInput} 
+        value={editInputs.aboutMe} 
+        placeholder='About Me'
+        rows={5} />
+
+        <label htmlFor="">Skills</label>
+        <input 
+        onChange={getInput} 
+        className="common-input" 
+        name="skills" 
+        placeholder='Skills' 
+        value={editInputs.skills} />        
       </div>
       <div className='save-container'>
         <button className='save-btn' onClick={updateProfileData}>Save</button>
